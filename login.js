cc89406fada7836794934a3e6ea3b884c5dc78ff
Juliet Araya <juliet.araya@inacapmail.cl>
2026-04-20 16:48:47 -0400

    html and js commit

---
+++ login.js
@@ -0,0 +1,42 @@
+const users = [
+    { email: "admin@club.com", password: "123", role: "admin" },
+    { email: "coach@club.com", password: "123", role: "coach" },
+    { email: "user@club.com", password: "123", role: "user" }
+];
+
+    function login(){
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        const user = users.find(u => u.email === email && u.password === password);
+        console.log("login ejecutado");
+        if(!user){
+            alert("Credenciales incorrectas");
+            return;
+        }
+
+    // 💾 GUARDAR SESIÓN
+        localStorage.setItem("user", JSON.stringify(user));
+
+    // 🔀 REDIRECCIÓN POR ROL
+        if(user.role === "admin"){
+            window.location.href = "admin.html";
+        }
+        else if(user.role === "coach"){
+            window.location.href = "coach.html";
+        }
+        else{
+            window.location.href = "user.html";
+        }
+        }
+
+        function logout(){
+            localStorage.removeItem("user");
+            window.location.href = "landing_page.html";
+        }
+
+        const user = JSON.parse(localStorage.getItem("user"));
+
+        if(user){
+            console.log("Bienvenido:", user.email);
+        }
